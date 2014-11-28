bcbe1d7c6090cee544349a0d344c1301cb1776bc
thynguyen <thynguyen222@gmail.com>
2014-11-28 05:24:15 -0800

    xxx

--- src/App/App.php
+++ src/App/App.php
@@ -1,39 +1,13 @@
 <?php
-/**
- * My Application sample
- *
- * @category SampleCiWithPhp
- * @package  App
- * @author   Marcelo Gornstein <marcelog@gmail.com>
- * @license  http://marcelog.github.com/ Apache License 2.0
- * @link     http://marcelog.github.com/
- */
+
 namespace App;
 
-/**
- * My Application sample
- *
- * @category SampleCiWithPhp
- * @package  App
- * @author   Marcelo Gornstein <marcelog@gmail.com>
- * @license  http://marcelog.github.com/ Apache License 2.0
- * @link     http://marcelog.github.com/
- */
 class App
 {
-    /**
-     * This will run the application.
-     *
-     * @return integer
-     */
     public function run()
     {
-		$anh_san_aaa_ddd_ddd_ddd = 4444;
-		$1 = 4444;
-		$2 = array('1','2','3',);
-		echo 'this is xxxx';
 		        
-		return $anhSan;
+		return 'xxx';
     }
 
 	public function xxx_aaa_aaa() {
